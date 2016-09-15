8d79ef70738b6d5adda435344c881a713aa8a203
Daniel Jimenez Garcia <daniel.garcia@oliverwyman.com>
2016-09-15 13:15:30 +0100

    Render markdown in article contents using a new tag helper

---
+++ src/BlogPlayground/TagHelpers/MarkdownTagHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using HeyRed.MarkdownSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogPlayground.TagHelpers
+{
+    public class MarkdownTagHelper : TagHelper
+    {
+        public string Source { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            //add wrapper span with well known class
+            output.TagName = "div";
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.Attributes.SetAttribute("class", "markdown-contents");
+
+            //Add processed markup as the inner contents. Notice the usage of SetHtmlContent so the processed markup is not encoded
+            var markdown = new Markdown();           
+            output.Content.SetHtmlContent(markdown.Transform(this.Source));
+        }        
+    }
+}
