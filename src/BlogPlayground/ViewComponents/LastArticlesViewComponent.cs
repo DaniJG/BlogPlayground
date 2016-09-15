83642436a5706fa493019a25c56b9519e0a8138c
Daniel Jimenez Garcia <daniel.garcia@oliverwyman.com>
2016-09-15 15:39:51 +0100

    Add view component to display latest articles

---
+++ src/BlogPlayground/ViewComponents/LastArticlesViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using BlogPlayground.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogPlayground.ViewComponents
+{
+    public class LastArticlesViewComponent: ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LastArticlesViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int howMany = 2)
+        {
+            var lastArticles = await _context.Article
+                                            .OrderByDescending(a => a.CreatedDate)
+                                            .Take(howMany)
+                                            .ToListAsync();
+            return View(lastArticles);
+        }
+    }
+}
